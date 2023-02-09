b406dfa2abe2c01022888b9b9a68ce32405856ac
Srabana Kumar Malla <srabankumarmalla1998@gmail.com>
2023-02-10 01:54:53 +0530

    Add files via upload

---
+++ Multinomial Naive Bayes.py
@@ -0,0 +1,141 @@
+#import the necessary libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#import the dataset
+dataset = pd.read_csv(r"C:\Users\dell\OneDrive\Documents\Data Science\3.Aug\3rd\Social_Network_Ads.csv")
+
+
+#split the dataset into D.V and I.V
+x = dataset.iloc[:,[2,3]].values
+y = dataset.iloc[:,-1].values
+
+
+#split the dataset into training set and test set
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.20,random_state=0)
+
+
+#feature scalling
+from sklearn.preprocessing import Normalizer
+sc = Normalizer()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+
+#training the Bernoulli Naive Bayes algorithm into the training set
+from sklearn.naive_bayes import MultinomialNB
+classifier = MultinomialNB()
+classifier.fit(x_train,y_train)
+
+
+#Predicting the Test set results
+y_pred = classifier.predict(x_test)
+
+                            
+#Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+print(cm)
+
+
+#This is to get the Model's Accuracy
+from sklearn.metrics import accuracy_score
+ac = accuracy_score(y_test,y_pred)
+print(ac)
+
+
+#This is to get the Classification Report
+from sklearn.metrics import classification_report
+cr = classification_report(y_test,y_pred)
+print(cr)
+
+
+#bias calculation
+bias = classifier.score(x_train,y_train)
+bias
+
+
+#variance calculation
+variance = classifier.score(x_test,y_test)
+variance
+
+
+#Visualising the Training set results
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_train, y_train
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(("red", "green")))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(("red", "green"))(i), label = j)
+plt.title("MNB (Training set)")
+plt.xlabel("Age")
+plt.ylabel("Estimated Salary")
+plt.legend()
+plt.show()
+
+
+#Visualising the Test set results
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_test, y_test
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(("red", "green")))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(("red", "green"))(i), label = j)
+plt.title("MNB (Test set)")
+plt.xlabel("Age")
+plt.ylabel("Estimated Salary")
+plt.legend()
+plt.show()
+
+
+#-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*#
+
+
+#------------------------FUTURE PREDICTION--------------------------
+
+
+#import the future prediction dataset
+dataset1 = pd.read_csv(r"C:\Users\dell\OneDrive\Documents\Data Science\3.Aug\3rd\Future prediction1.csv")
+
+
+#copy the future prediction dataset in to a new variable
+d2 = dataset1.copy()
+
+
+#clean the future prediction dataset for the further operation
+dataset1 = dataset1.iloc[:,[2, 3]].values
+
+
+#Feature Scalling of the future prediction dataset
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler() 
+M = sc.fit_transform(dataset1)
+
+
+#creating the future prediction dataframe
+y_pred_MultinomialNB = pd.DataFrame()
+
+d2["y_pred_MultinomialNB"] = classifier.predict(M)
+
+
+#save the future prediction dataframe as the .csv file format
+d2.to_csv("FPofMultinomialNBAlgo.csv")
+
+
+#To get the path where exactly the predicted .csv file saved in our desktop
+import os
+os.getcwd()
+
